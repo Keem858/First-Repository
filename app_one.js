93d9f5cea3596dcee8633cf97da6829cdfda73ea
Keem858 <elwayprime007@gmail.com>
2022-03-19 23:24:32 -0400

    end of 3.19.22
    Got the random answer choices working with the correct answer... Next, I need to figure out how to generate the wrong answers and fill them into the other choices.

--- app_one.js
+++ app_one.js
@@ -1,64 +1,22 @@
 var colorGeneratorCounter = 0;
 var prevRng;
 
-/*
-//generates a random color from a number
-function colorGenerator(){
-    var colorContainer = document.querySelector(".color-container");
-    var colorArr = ["","","","","","","","","","","","","","","","","","","","","","","","","",""];
-    var rng = Math.floor(Math.random() * 6);
-    //make a if statement to make sure that colors are not repeated
-    if(rng == prevRng){
-        while(rng == prevRng){
-            rng = Math.floor(Math.random() * 6)
-        }
-    }
-    console.log(rng);
-
-    var assignColor = colorArr[rng];
-
-    colorContainer.style.backgroundColor = assignColor;
-
-    colorGeneratorCounter = colorGeneratorCounter + 1;
-    prevRng = rng;
-}
-
-//function to keep track if the click is even or odd
-function evenOrOdd(counter){
-    if(counter % 2 == 0){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-//generate the question based on colorGeneratorCounter being even or odd
-function generateQuestion(){
-    var questionText = document.querySelector(".question-text");
-
-    var questionArr = ["What is the hexadecimal value of this color?",
-    "What is the rgb value of this color?"];
-
-    questionText.innerHTML = '';
-
-    if(evenOrOdd(colorGeneratorCounter) == true){
-        questionText.append(questionArr[0]);
-    }else{
-        questionText.append(questionArr[1]);
-    }
-}
-*/
-
-function colorGenerator(){
-    var colorContainer = document.querySelector(".color-container"); //color-container from the html css
-
+function createHexaColor(){
     var maxColorVal = 0xFFFFFF; //16777215
     var randomNumber = Math.floor(Math.random() * maxColorVal);
     randomNumber = randomNumber.toString(16);
     randColor = randomNumber.padStart(6,0);
     randColor = `#${randColor.toUpperCase()}`;
 
+    return(randColor);
+}
+
+function colorGenerator(){
+    var colorContainer = document.querySelector(".color-container"); //color-container from the html css
+
+    let randColor = createHexaColor();
     console.log(randColor);
+
     colorContainer.style.backgroundColor = randColor;
 
     colorGeneratorCounter++;
@@ -87,4 +45,33 @@ function generateQuestion(){
     }else{
         questionText.append(questionArr[1]);
     }
+}
+
+//function to assign an answer to the four answer choice buttons
+function answerChoiceButtons(){
+    //putting the answer choice buttons into js
+    var choiceOne = document.querySelector(".choice-one");
+    var choiceTwo = document.querySelector(".choice-two");
+    var choiceThree = document.querySelector(".choice-three");
+    var choiceFour = document.querySelector(".choice-four");
+    var randAnswer;
+
+    let randColor = createHexaColor();
+
+    //pick random button to put the color answer into
+    let randNum = Math.floor(Math.random() * 4);
+    
+    if(randNum == 0){
+        randAnswer = choiceOne;
+    }else if(randNum == 1){
+        randAnswer = choiceTwo;
+    }else if(randNum == 2){
+        randAnswer = choiceThree;
+    }else{
+        randAnswer = choiceFour;
+    }
+
+    randAnswer.append(" " + randColor);
+
+    //fill in the other choices with random hexadecimal
 }
